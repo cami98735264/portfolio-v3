4ef2aad8ca7c1f8b1b2cac0c3a921d0c3c52a4ec
cami98735264 <cr7montealegre2010@gmail.com>
2025-01-15 23:06:59 -0500

    First commit

---
+++ frontend/src/utils/navbarElements.ts
@@ -0,0 +1,18 @@
+let navbarElements = {
+	links: [
+		{
+			name: 'Proyectos',
+			url: '/proyectos'
+		},
+		{
+			name: 'Tecnologías',
+			url: '/tecnologias'
+		},
+		{
+			name: 'Experiencia',
+			url: '/experiencia'
+		}
+	]
+}
+
+export default navbarElements;
