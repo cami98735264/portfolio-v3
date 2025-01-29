6dc811b759ffe011cf35c1f9bf2bfc3531e05bff
cami98735264 <cr7montealegre2010@gmail.com>
2025-01-29 17:03:09 -0500

    Add Node adapter for server output in Astro configuration

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
+import node from "@astrojs/node";
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), icon()]
+  integrations: [react(), icon()],
+  output: "server",
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
